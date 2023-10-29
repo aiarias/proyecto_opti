f3d2eaa22cd67040f9bcdf544c060cbe8a27ead6
Javiera Pinto Santa Maria <javierapintosantamaria@MacBook-Pro-de-Javiera.local>
2023-10-29 19:49:15 -0300

    error

--- main.py
+++ main.py
@@ -37,19 +37,20 @@ emisiones_dict = dict(zip(df_emisiones['Tipo de Vehículo / Combustible'], df_em
 df_capacidad = pd.read_excel("Datos.xlsx", sheet_name="Capacidad")
 
 # Crear un diccionario con los nombres de los vehículos como claves y sus capacidades como valores
-capacidad_dict = dict(zip(df_capacidad['Tipo de Vehículo / Combustible'], df_capacidad['Capacidad (unidades)']))
+capacidad_dict = dict(zip(df_capacidad['Tipo de Vehiculo / Combustible'], df_capacidad['Capacidad (unidades)']))
 
 
 # Conjuntos
 A = list(emisiones_dict.keys()) # conjunto de vehículos
-M = list(range(101)) # conjunto de paquetes
+M = list(['Camino Lo Boza, 120, Pudahuel, Chile']) # empresas
 J = direcciones # conjunto de ubicaciones
 L = 0 # tiempo de contrato
 I = range(37) # paquetes de la empresa m
 
-print(A)
-print(M)
+# print(A)
+# print(M)
 print(J)
+print(D['Camino Lo Boza, 120, Pudahuel, Chile', 'Glaciar Aguila Dos 2592, MAIPÚ, Chile'])
 
 # Parámetros
 N = 100 # cantidad de paquetes de la empresa m
@@ -69,7 +70,7 @@ model = Model()
 # Creamos Variables de decision 
 
 # Variables
-X = model.addVars(A, M, J, vtype=GRB.BINARY, name="X")
+X = model.addVars(A, I, J, vtype=GRB.BINARY, name="X")
 W = model.addVars(A, J, J, vtype=GRB.BINARY, name="W")
 Z = model.addVars(A, vtype=GRB.BINARY, name="Z")
 Y = model.addVars(J, vtype=GRB.CONTINUOUS, name="Y")
@@ -80,41 +81,55 @@ model.update()
 # Restricciones
 
 # 1. Restricción de capacidad
-for a in A:
-    for j in J:
-        model.addConstr(quicksum(X[a,m,i,j] for m in M for i in I) <= C[a])
+#for a in A:
+#    for j in J:
+#        model.addConstr(quicksum(X[a,m,i,j] for m in M for i in I) <= C[a])
+
+model.addConstrs((C[a] >= quicksum(X[a,i,j] for i in I) for a in A for j in J), name="R1")
 
 # 2. Cada paquete es transportado por un solo vehículo
-for m in M:
-    for i in I:
-        for j in J:
-            model.addConstr(quicksum(X[a,m,i,j] for a in A) == 1)
+# for m in M:
+#     for i in I:
+#         for j in J:
+#             model.addConstr(quicksum(X[a,m,i,j] for a in A) == 1)
+
+model.addConstrs((1 == quicksum(X[a,i,j] for a in A) for i in I for j in J), name="R2")
 
 # 3. Cada ubicación de entrega es atendida una vez
-for i in I:
-    model.addConstr(quicksum(W[a,i,j] for a in A for j in J) == 1)
+# for i in I:
+#     model.addConstr(quicksum(W[a,i,j] for a in A for j in J) == 1)
+
+model.addConstrs((1 == quicksum(X[a,i,j] for a in A for j in J) for i in I), name="R3")
 
 # 4. Restricción de flujo
-for a in A:
-    for i in I:
-        model.addConstr(quicksum(W[a,j,i] for j in J) == 1)
-        model.addConstr(quicksum(W[a,i,j] for j in J) == 1)
+# for a in A:
+#     for i in I:
+#         model.addConstr(quicksum(W[a,j,i] for j in J) == 1)
+#         model.addConstr(quicksum(W[a,i,j] for j in J) == 1)
+model.addConstrs((1 == quicksum(W[a,j1,j2] for j1 in J) for j2 in J for a in A), name="Flujo1")
+model.addConstrs((1 == quicksum(W[a,j1,j2] for j2 in J) for j1 in J for a in A), name="Flujo2")
+
 
 # 5. Restricción de recorrido
-for a in A:
-    for i in range(1, len(I)):
-        for j in range(i+1, len(J)+1):
-            model.addConstr(Y[a,i] - Y[a,j] + len(J)*W[a,i,j] <= len(J) - 1)
+# for a in A:
+#     for i in range(1, len(I)):
+#         for j in range(i+1, len(J)+1):
+#             model.addConstr(Y[i] - Y[j] + len(J)*W[a,i,j] <= len(J) - 1)
+
+#model.addConstrs((Y[j1] - Y[j2] + len(J)*W[a,j1,j2] <= len(J) - 1 for a in A for j1 in J for j2 in J if j1 != j2 if j1 >= 1 if j2 >= 1 if j1 <= (len(J) + 1) if j2 <= (len(J) + 1)), name="R5")
 
 # 6. Restricción de tiempo
-for a in A:
-    model.addConstr(T - quicksum(X[a,m,i,j]*D[m,i] + W[a,i,j]*D[i,j] for m in M for i in I for j in J) + M*Z[a] >= 1)
-    model.addConstr(quicksum(X[a,m,i,j]*D[m,i] + W[a,i,j]*D[i,j] for m in M for i in I for j in J) - T + M*(1-Z[a]) >= 0)
+# for a in A:
+#     model.addConstr(T - quicksum(X[a,m,i,j]*D[m,i] + W[a,i,j]*D[i,j] for m in M for i in I for j in J) + M*Z[a] >= 1)
+#     model.addConstr(quicksum(X[a,m,i,j]*D[m,i] + W[a,i,j]*D[i,j] for m in M for i in I for j in J) - T + M*(1-Z[a]) >= 0)
+
+#model.addConstrs((1 < T - quicksum(X[a,i,m,j] * )))
 
 
 # Funcion objetivo
 
-objetivo = quicksum(X[a,m,i,j]*D[m,i]*E[a] + W[a,i,j]*D[i,j]*E[a] for a in A for m in M for i in I for j in J)
+#objetivo = quicksum(X[a,m,j]*D[m,i]*E[a] + W[a,i,j]*D[i,j]*E[a] for a in A for m in M for i in I for j in J)
+objetivo = quicksum(X[a,i,j] * D[m,j] * E[a] for m in M for a in A for i in I for j in J if j != m) + quicksum(W[a,j1,j2] * D[j1,j2] * E[a] for a in A for j1 in J for j2 in J)
 model.setObjective(objetivo, GRB.MINIMIZE)
 
 
