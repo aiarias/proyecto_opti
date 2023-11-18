e8dec11a0d8986354d31f0408b59971189e799b1
Javiera Pinto Santa Maria <javierapintosantamaria@MacBook-Pro-de-Javiera.local>
2023-11-18 00:29:02 -0300

    hay un error con la R6

--- main.py
+++ main.py
@@ -42,15 +42,14 @@ capacidad_dict = dict(zip(df_capacidad['Tipo de Vehiculo / Combustible'], df_cap
 
 # Conjuntos
 A = list(emisiones_dict.keys()) # conjunto de vehículos
-M = list(['Camino Lo Boza, 120, Pudahuel, Chile']) # empresas
-J = direcciones # conjunto de ubicaciones
+# M = list(['Camino Lo Boza, 120, Pudahuel, Chile']) # empresas
+I = direcciones # conjunto de ubicaciones
 L = 0 # tiempo de contrato
-I = range(37) # paquetes de la empresa m
 
 # print(A)
-print(M)
-print(J)
-print(D['Camino Lo Boza, 120, Pudahuel, Chile', 'Camino Lo Boza, 120, Pudahuel, Chile'])
+# print(M)
+# print(J)
+# print(D['Camino Lo Boza, 120, Pudahuel, Chile', 'Camino Lo Boza, 120, Pudahuel, Chile'])
 
 # Parámetros 
 N = 37 # cantidad de paquetes de la empresa m
@@ -61,7 +60,7 @@ C = capacidad_dict # capacidad máxima de cada vehículo
 t = 0.0263 # velocidad promedio de un auto en recorrer un kilometro
 T = 8.3 # tiempo total de las entregas
 p = 4500 # pago extra
-Q = 10**10
+M = 10**10
 
 # Crear un modelo vacio
 
@@ -70,10 +69,16 @@ model = Model()
 # Creamos Variables de decision 
 
 # Variables
-X = model.addVars(A, I, J, vtype=GRB.BINARY, name="X")
-W = model.addVars(A, J, J, vtype=GRB.BINARY, name="W")
-Z = model.addVars(A, vtype=GRB.BINARY, name="Z")
-Y = model.addVars(J, vtype=GRB.CONTINUOUS, name="Y")
+Y = model.addVars(A, I, vtype=GRB.BINARY, name="Y")
+X = model.addVars(A, I, I, vtype=GRB.BINARY, name = "X")
+Z = model.addVars(A, vtype=GRB.BINARY, name = "Z")
+R = model.addVars(I, vtype=GRB.CONTINUOUS, name = "R")
+U = model.addVars(A, I, vtype=GRB.INTEGER, name = "U")
+
+# X = model.addVars(A, I, J, vtype=GRB.BINARY, name="X")
+# W = model.addVars(A, J, J, vtype=GRB.BINARY, name="W")
+# Z = model.addVars(A, vtype=GRB.BINARY, name="Z")
+# Y = model.addVars(J, vtype=GRB.CONTINUOUS, name="Y")
 
 # Llama al update para agregar las variables al modelo
 model.update()
@@ -82,68 +87,50 @@ model.update()
 
 # 1. Restricción de capacidad
 
-model.addConstrs((C[a] >= quicksum(X[a,i,j] for i in I) for a in A for j in J), name="R1")
+# model.addConstrs((C[a] >= quicksum(X[a,i,j] for i in I) for a in A for j in J), name="R1")
+model.addConstrs((C[a] >= quicksum(Y[a, i] for i in I) for a in A), name = "R1")
 
 # 2. Cada paquete es transportado por un solo vehículo
 
-model.addConstrs((1 == quicksum(X[a,i,j] for a in A) for i in I for j in J), name="R2")
+# model.addConstrs((1 == quicksum(X[a,i,j] for a in A) for i in I for j in J), name="R2")
+model.addConstrs((1 == quicksum(Y[a, i] for a in A) for i in I), name = "R2")
 
 # 3. Cada ubicación de entrega es atendida una vez
 
-model.addConstrs((1 == quicksum(X[a,i,j] for a in A) for i in I for j in J), name="R3")
+# model.addConstrs((1 == quicksum(X[a,i,j] for a in A) for i in I for j in J), name="R3")
+model.addConstrs((1 == quicksum(X[a, i, j] for a in A for j in I) for i in I), name = "R3")
 
 # 4. Restricción de flujo
 
-model.addConstrs((1 == quicksum(W[a,j1,j2] for j1 in J) for j2 in J for a in A), name="Flujo1")
-model.addConstrs((1 == quicksum(W[a,j1,j2] for j2 in J) for j1 in J for a in A), name="Flujo2")
+# model.addConstrs((1 == quicksum(W[a,j1,j2] for j1 in J) for j2 in J for a in A), name="Flujo1")
+# model.addConstrs((1 == quicksum(W[a,j1,j2] for j2 in J) for j1 in J for a in A), name="Flujo2")
+model.addConstrs((1 == quicksum(X[a, i, j] for i in I) - quicksum(X[a, j, i] for i in I if i != 'Camino Lo Boza, 120, Pudahuel, Chile') for j in I for a in A if i == 'Camino Lo Boza, 120, Pudahuel, Chile'), name = "Flujo1")
+model.addConstrs((0 == quicksum(X[a, i, j] for i in I) - quicksum(X[a, j, i] for i in I if i != 'Camino Lo Boza, 120, Pudahuel, Chile') for j in I for a in A if i == 'Camino Lo Boza, 120, Pudahuel, Chile'), name = "Flujo2")
 
 
-#5. Restricción de recorrido
-# for a in A:
-#     for i in range(1, len(I)):
-#         for j in range(i+1, len(J)+1):
-#             model.addConstr(Y[i] - Y[j] + len(J)*W[a,i,j] <= len(J) - 1)
+#5. Sub-tours
 
-# model.addConstrs((Y[j1] - Y[j2] + len(J)*W[a,j1,j2] <= len(J) - 1 for a in A for j1 in J for j2 in J if j1 != j2 if j1 >= 1 if j2 >= 1 if j1 <= (len(J) + 1) if j2 <= (len(J) + 1)), name="R5")
-
-# J_indices = list(range(1, len(J)+1))
-# model.addConstrs((Y[j1] - Y[j2] + len(J_indices)*W[a,j1,j2] <= len(J_indices) - 1 
-#                   for a in A for j1 in J_indices for j2 in J_indices if j1 != j2), name="R5")
+model.addConstrs((C[a] - 1 >= U[a, i] - U[a, j] + C[a]*X[a, i, j] for i in I for j in I for a in A), name = "Sub-tours1")
+model.addConstrs((C[a] >= U[a, j] for j in I for a in A), name = "Sub-tours2")
+model.addConstrs((1 <= U[a, j] for j in I for a in A), name = "Sub-tours3")
 
 
 # 6. Restricción de tiempo
-# for a in A:
-#     model.addConstr(T - quicksum(X[a,m,i,j]*D[m,i] + W[a,i,j]*D[i,j] for m in M for i in I for j in J) + M*Z[a] >= 1)
-#     model.addConstr(quicksum(X[a,m,i,j]*D[m,i] + W[a,i,j]*D[i,j] for m in M for i in I for j in J) - T + M*(1-Z[a]) >= 0)
 
-#model.addConstrs((1 < T - quicksum(X[a,i,m,j] * )))
+model.addConstrs((1 < T - quicksum(X[a, i, j]*D[i, j]*t for i in I for j in I) + M*Z[a] for a in A), name = "Tiempo1")
+model.addConstrs((0 < quicksum(X[a, i, j]*D[i, j]*t for i in I for j in I) - T + M*(1 - Z[a]) for a in A), name = "Tiempo2")
 
-# model.addConstrs((1 < T - quicksum(X[a,i,j] * D[m,j] * t for m in M or i in I for j in J) + quicksum(W[a,j1,j2] * D[j1,j2] * t for j1 in J for j2 in J) + Q * Z[a] for a in A), name="Tiempo1")
-# model.addConstrs((0 < quicksum(X[a,i,j] * D[m,j] * t for m in M or i in I for j in J) + quicksum(W[a,j1,j2] * D[j1,j2] * t for j1 in J for j2 in J) - T + Q*(1 - Z[a]) for a in A),name="Tiempo2")
-# R6a: Si el tiempo total de entregas supera el tiempo de contrato T para vehículo a
-# print(W.keys(
-#              ))
 
-# for a in A:
-#     model.addConstr(
-#         quicksum(quicksum(X[a, i, m] * D[m, j] * t for j in J for m in M) for i in I) + 
-#         quicksum(quicksum(W[a, i, j] * D[i, j] * t for j in J) for i in I) +
-#         M * Z[a] >= T + 1, 
-#         name="R6a_{}".format(a)
-#     )
+# 7. Relación entre variables
 
-# # R6b: Si el tiempo total de entregas es menor o igual al tiempo de contrato T para vehículo a
-# for a in A:
-#     model.addConstr(
-#         quicksum(quicksum(X[a, i, m] * D[m, j] * t for j in J for m in M) for i in I) + 
-#         quicksum(quicksum(W[a, i, j] * D[i, j] * t for j in J) for i in I) -
-#         T + M * (1 - Z[a]) > 0, 
-#         name="R6b_{}".format(a)
-#     )
+model.addConstrs((Y[i, a] == quicksum(X[a, i, j] for a in A) for i in I for j in I), name = "Relación1")
+model.addConstrs((Y[a, 'Camino Lo Boza, 120, Pudahuel, Chile'] == quicksum(X[a, i, j] for i in I) for a in A for j in I), name = "Relación2")
+
+
 # Funcion objetivo
 
 #objetivo = quicksum(X[a,m,j]*D[m,i]*E[a] + W[a,i,j]*D[i,j]*E[a] for a in A for m in M for i in I for j in J)
-objetivo = quicksum(X[a,i,j] * D[m,j] * E[a] for m in M for a in A for i in I for j in J if j != m) + quicksum(W[a,j1,j2] * D[j1,j2] * E[a] for a in A for j1 in J for j2 in J)
+objetivo = quicksum(X[a, i, j]*D[i, j]*E[a] for a in A for i in I for j in I)
 model.setObjective(objetivo, GRB.MINIMIZE)
 
 
@@ -173,3 +160,4 @@ for a in A:
     if Z[a].x == 1:
         print(f"El vehículo {a} sobrepasó el tiempo de contrato {T}")
     recorrido_total.append(recorrido)
+print(recorrido_total)
